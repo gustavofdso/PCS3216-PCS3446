d7e0b349109fa2060e198348582610abfb360708
Gustavo Oliveira <gustavodesa.10@gmail.com>
2022-05-27 23:26:26 -0300

    imported classes

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.VirtualMachine import VirtualMachine
+
+virtualmachine = VirtualMachine()
--- src/VirtualMachine.py
+++ src/VirtualMachine.py
@@ -1,4 +1,12 @@
+from re import L
+from src.Assembler import Assembler
+from src.Loader import Loader
+from src.Dumper import Dumper
+from src.Linker import Linker
 
 class VirtualMachine:
     def __init__(self):
-        pass
+        self.assembler = Assembler()
+        self.loader = Loader()
+        self.dumper = Dumper()
+        self.linker = Linker()
