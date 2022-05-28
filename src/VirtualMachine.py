90f9ae0ad7798239e25913ca00eb031d10a800bd
Gustavo Oliveira <gustavodesa.10@gmail.com>
2022-05-28 16:15:31 -0300

    started CPU

--- src/VirtualMachine.py
+++ src/VirtualMachine.py
@@ -1,3 +1,5 @@
+from src.CPU import CPU
+
 from src.Assembler import Assembler
 from src.Loader import Loader
 from src.Dumper import Dumper
@@ -5,13 +7,12 @@ from src.Linker import Linker
 
 class VirtualMachine:
     def __init__(self):
+        # Initializing CPU
+        self.CPU = CPU
+
+        # Initializing system programms
         self.assembler = Assembler()
         self.loader = Loader()
         self.dumper = Dumper()
         self.linker = Linker()
 
-        self.pc
-        self.ri
-        self.accumulator
-        
-        self.memory
