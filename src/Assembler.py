85b52647b121a3b98f7143aa558b779ff2f3975f
Gustavo Oliveira <gustavodesa.10@gmail.com>
2022-06-09 15:01:24 -0300

    started assembler

--- src/Assembler.py
+++ src/Assembler.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from ctypes import c_uint8, c_uint16, c_int8
 
 class AssemblyError(Exception):
@@ -7,43 +8,70 @@ class AssemblyError(Exception):
 class Assembler:
     
     def __init__(self):
-        self.mnemonics_table = {
-            "JP": 0x0,
-            "JZ": 0x1,
-            "JN": 0x2,
-            "LV": 0x3,
-            "+" : 0x4,
-            "-" : 0x5,
-            "*" : 0x6,
-            "/" : 0x7,
-            "LD": 0x8,
-            "MM": 0x9,
-            "SC": 0xA,
-            "RS": 0xB,
-            "HM": 0xC,
-            "GD": 0xD,
-            "PD": 0xE,
-            "OS": 0xF,
-        }
 
-        self.pseudoinstrucions = ['@', '#', '$', 'K', '&', '>', '<']
+
+        self.pseudoinstrucions_list = ['@', '#', '$', 'K', '&', '>', '<']
 
     def assemble(self, filename):
+
+        # Opening the file and reading lines
         with open(filename, 'r') as f:
             file_lines = f.readlines()
 
-        label_table = {}
-        object_code = []
-        instruction_counter = 0
-        initial_address = 0
-        current_object_file = 0
+        # Separating commands from comments
+        lines = pd.DataFrame(columns = ['instruction', 'comment'])
+        for line in file_lines:
+            if ';' in line:
+                instruction = line.split(';', maxsplit = 1)[0].strip()
+                comment = line.split(';', maxsplit = 1)[1].strip()
+            else:
+                instruction = line.strip()
+                comment = ''
 
-        lines = []
-        for i, line in enumerate(file_lines):
-            instruction = line.split(';', maxsplit = 1)[0].strip()
-            comment = ''.join(line.split(';', maxsplit = 1)[1:]).strip()
-            if instruction == '': continue
-            
-            lines.append((i, instruction, comment))
+            lines.loc[lines.shape[0]] = [instruction, comment]
+
+        # Executing assembly steps
+        for step in [1, 2]:
+            instruction_counter = 0
+
+            for i in lines.index:
+                binary = self.binary_from_instruction(instruction)
+                instruction_counter += 1
 
         save_path = '\\object\\' + '.'.join(filename.split('.')[:-1]) + 'obj'
+
+    def binary_from_instruction(self, instruction):
+        splited_instruction = instruction.split()
+        if len(splited_instruction) == 0: return ''
+
+        if splited_instruction[0] == 'JP':
+
+        elif splited_instruction[0] == 'JZ':
+
+        elif splited_instruction[0] == 'JN':
+
+        elif splited_instruction[0] == 'LV':
+
+        elif splited_instruction[0] == '+':
+
+        elif splited_instruction[0] == '-':
+
+        elif splited_instruction[0] == '*':
+
+        elif splited_instruction[0] == '/':
+
+        elif splited_instruction[0] == 'LD':
+
+        elif splited_instruction[0] == 'MM':
+
+        elif splited_instruction[0] == 'SC':
+
+        elif splited_instruction[0] == 'RS':
+            
+        elif splited_instruction[0] == 'HM':
+
+        elif splited_instruction[0] == 'GD':
+
+        elif splited_instruction[0] == 'PD':
+
+        elif splited_instruction[0] == 'OS':
