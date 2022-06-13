7a71507bd323aa21b32f108c5f02d192734714f4
Gustavo Oliveira <gustavodesa.10@gmail.com>
2022-06-12 21:41:05 -0300

    did more instructions

--- src/VirtualMachine.py
+++ src/VirtualMachine.py
@@ -47,10 +47,13 @@ class VirtualMachine:
     def fetch_instruction(self):
         pass
 
+    def execute_instruction(self):
+        pass
+
     def run(self, step = True):
         self.instruction_register = self.main_memory[0][0x022].value << 8 | self.main_memory[0][0x023].value
         self.running = True
         while self.running:
             self.fetch_instruction()
-            self.decode_execute()
+            self.execute_instruction()
             if step: input()
