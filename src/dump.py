e06a57b3c8b8b92d0f7ee3f64cc240bd6b9ec382
Gustavo Oliveira <gustavodesa.10@gmail.com>
2022-06-22 18:52:46 -0300

    improved dumper and loader to not overflow

--- src/dump.py
+++ src/dump.py
@@ -5,6 +5,7 @@ def dump(self, filename, size, start_adress, bank):
 
     obj_code = '{:b}'.format(bank).zfill(4) + '{:b}'.format(start_adress).zfill(12) + '\n'
     for i in range(size):
+        if start_adress + i >= len(self.memory[bank]): break
         obj_code += '{:b}'.format(self.memory[bank][start_adress + i].value).zfill(8) + '\n'
 
     # Opening the file and dumping the memory lines
@@ -19,6 +20,7 @@ def hex_dump(self, size, start_adress, bank):
 
     print('Memory bank: {:d}'.format(bank))
     for i in range(size):
+        if start_adress + i >= len(self.memory[bank]): break
         print(
             '{:X}'.format(start_adress + i).zfill(2),
             '=>',
