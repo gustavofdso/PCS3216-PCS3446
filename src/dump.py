dfe935f721cace20bf5cba303d7242e2a92ab951
gustavofdso <106266585+gustavofdso@users.noreply.github.com>
2022-06-24 07:21:49 -0300

    Update dump.py

--- src/dump.py
+++ src/dump.py
@@ -1,3 +1,5 @@
+def DumpError(Exception): pass
+
 def dump(self, filename, size, start_adress, bank):
     start_adress = self.process_operator(start_adress)
     bank = self.process_operator(bank)
@@ -5,7 +7,7 @@ def dump(self, filename, size, start_adress, bank):
 
     obj_code = '{:04b}'.format(bank) + '{:012b}'.format(start_adress) + '\n'
     for relative_adress in range(size):
-        if start_adress + relative_adress >= len(self.memory[bank]): break
+        if start_adress + relative_adress >= len(self.memory[bank]): raise(DumpError("Memory overflow on dump!"))
         obj_code += '{:08b}'.format(self.memory[bank][start_adress + relative_adress].value) + '\n'
 
     # Opening the file and dumping the memory lines
@@ -17,12 +19,12 @@ def hex_dump(self, size, start_adress, bank):
     start_adress = self.process_operator(start_adress)
     bank = self.process_operator(bank)
     size = self.process_operator(size)
-
+    
     print('Memory bank: {:02d}'.format(bank))
     for relative_adress in range(size):
-        if start_adress + relative_adress >= len(self.memory[bank]): break
+        if start_adress + relative_adress >= len(self.memory[bank]): raise(DumpError("Memory overflow on dump!"))
         print(
-            '{:02X}'.format(start_adress + relative_adress),
+            '{:03X}'.format(start_adress + relative_adress),
             '=>',
             '{:02X}'.format(self.memory[bank][start_adress + relative_adress].value)
         )
