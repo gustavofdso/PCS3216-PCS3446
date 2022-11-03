b2810019a83e7cee057d5b47384ef6f7240a74ce
Gustavo Oliveira <gustavo.oliveira@alaska-asset.com.br>
2022-11-03 16:34:39 -0300

    execution plot

--- src/jobscheduler.py
+++ src/jobscheduler.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 
 # Running job scheduler
@@ -6,15 +7,16 @@ def job_scheduler(self, filename, n, i, o):
     df = pd.read_csv('./jobs/' + filename + '.csv')
     df['duration'] += df['in']*i
     df['duration'] += df['out']*o
-    df['left'] = df['duration']
+    df['begin'], df['end'], df['left'] = 0, 0, df['duration']
 
     time = 0
-    df.sort_values('arrival', inplace = True)
     memory = pd.DataFrame(columns = ['time', 'process', 'memory'])
     while not (df['left'] == 0).all():
         # Running n processes
         processes = df[(df['arrival'] <= time) & (df['left'] != 0)].head(n)
+        processes.loc[processes['left'] == processes['duration'], 'begin'] = time
         processes['left'] -= 1
+        processes.loc[processes['left'] == 0, 'end'] = time
         df = pd.concat([processes, df[~df.index.isin(processes.index)]])
 
         # Calculating used memory
@@ -25,13 +27,34 @@ def job_scheduler(self, filename, n, i, o):
         # Incrementing time
         time += max(1, len(processes))
 
-    # Plotting memory graph
-    memory = memory.pivot_table(index = 'time', columns = 'process', values = 'memory')
-    memory.plot.area(
-        title = f'Multiprogramação - n = {n}',
+    # Plotting processing
+    df.sort_index(ascending = False, inplace = True)
+    df['end'] -= df['begin']
+    df.plot.barh(
+        title = f'Evolução de execução - n = {n}',
+        y = ['begin', 'end'],
+        xlabel = 'Ciclos',
+        ylabel = 'Processo',
+        stacked = True,
+        grid = True,
+        linewidth = 0,
+        legend = None,
+        color = {
+            'begin': matplotlib.colors.to_rgba('orange', 0),
+            'end': matplotlib.colors.to_rgba('orange', 1)
+        }
+    )
+
+    # Plotting memory
+    memory.pivot_table(
+        index = 'time',
+        columns = 'process',
+        values = 'memory'
+    ).plot.area(
+        title = f'Uso de memória - n = {n}',
         xlabel = 'Ciclos',
         ylabel = 'Memória',
         grid = True,
-        linewidth = 0,
+        linewidth = 0
     )
     plt.show()
